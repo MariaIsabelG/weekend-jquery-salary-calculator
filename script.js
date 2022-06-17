b78a4b67bedf7df38cef66cfcef660d0244773a7
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-06-17 16:29:19 -0500

    created a function that gets an input and gives an output

--- script.js
+++ script.js
@@ -3,9 +3,16 @@ $( document ).ready( onReady );
 
 function onReady(){
     console.log( 'jQuery is loaded' );
-
+$( '#add-btn' ).on( 'click', addEmployee );
 
 
 }
 
 // input that collects data employee first name, last name, ID number, job title, annual salary
+function addEmployee(){
+    let name = $( '#name-in' ).val();
+    let lastName = $( '#last-name')
+    $( '#table-name' ).append( `<td>${name}</td>`);
+    $( '#name-in' ).val( '' );
+
+}
