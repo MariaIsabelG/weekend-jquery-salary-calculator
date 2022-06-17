5aeea9d12d262d2adc46af5750f53379778154fe
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-06-17 18:57:17 -0500

    delete button function is now working

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ $( document ).ready( onReady );
 function onReady(){
     console.log( 'jQuery is loaded' );
 $( '#add-btn' ).on( 'click', addEmployee );
+$( 'tbody' ).on( 'click', '.delete-btn', deleteEmployee );
 
 
 }
@@ -19,7 +20,8 @@ function addEmployee(){
     let identify = $( '#identify' ).val();
     let jobTitle = $( '#job-title' ).val();
     let salary = $( '#salary' ).val();
-    let addNewEmployee = ` <tr><td>${name}</td>, <td>${lastName}</td>, <td>${identify}</td>, <td>${jobTitle}</td>, <td>${salary}</td></tr>`;
+
+    let addNewEmployee = `<tr class="new-row"><td>${name}</td>, <td>${lastName}</td>, <td>${identify}</td>, <td>${jobTitle}</td>, <td>${salary}</td>, <td><button class="delete-btn">DELETE</button></td></tr>`;
 
 $( 'tbody' ).append( addNewEmployee );
 
@@ -29,3 +31,9 @@ $( '#identify' ).val( '' );
 $( '#job-title' ).val( '' );
 $( '#salary' ).val( '' );
 }
+
+function deleteEmployee(){
+
+ $(this).closest ('tr').remove ();
+
+};
