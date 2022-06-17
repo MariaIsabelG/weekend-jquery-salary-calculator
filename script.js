db817062ddb155d0d71a4e5173413f86b5f89e36
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-06-17 16:34:03 -0500

    finished addEmployee function ready to test

--- script.js
+++ script.js
@@ -11,8 +11,24 @@ $( '#add-btn' ).on( 'click', addEmployee );
 // input that collects data employee first name, last name, ID number, job title, annual salary
 function addEmployee(){
     let name = $( '#name-in' ).val();
-    let lastName = $( '#last-name')
+    let lastName = $( '#last-name').val();
+    let identify = $( '#identify' ).val();
+    let jobTitle = $( '#job-title' ).val();
+    let salary = $( '#salary' ).val();
+
     $( '#table-name' ).append( `<td>${name}</td>`);
+    $( '#name-in' ).val( '' );
+
+    $( '#table-last-name' ).append( `<td>${lastName}</td>`);
+    $( '#name-in' ).val( '' );
+
+    $( '#table-identify' ).append( `<td>${identify}</td>`);
+    $( '#name-in' ).val( '' );
+
+    $( '#table-title' ).append( `<td>${jobTitle}</td>`);
+    $( '#name-in' ).val( '' );
+
+    $( '#table-salary' ).append( `<td>${salary}</td>`);
     $( '#name-in' ).val( '' );
 
 }
