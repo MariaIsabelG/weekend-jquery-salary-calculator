05434a1a0d93ecb03d57305c9415d4f8c2023eb0
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-06-17 17:43:37 -0500

    tested and running

--- script.js
+++ script.js
@@ -10,25 +10,21 @@ $( '#add-btn' ).on( 'click', addEmployee );
 
 // input that collects data employee first name, last name, ID number, job title, annual salary
 function addEmployee(){
+
+    //let newEmployee = [name, lastName, identify, jobTitle, salary] 
+
     let name = $( '#name-in' ).val();
     let lastName = $( '#last-name').val();
     let identify = $( '#identify' ).val();
     let jobTitle = $( '#job-title' ).val();
     let salary = $( '#salary' ).val();
+    let addNewEmployee = ` <tr><td>${name}</td>, <td>${lastName}</td>, <td>${identify}</td>, <td>${jobTitle}</td>, <td>${salary}</td></tr>`;
 
-    $( '#table-name' ).append( `<td>${name}</td>`);
-    $( '#name-in' ).val( '' );
+$( 'tbody' ).append( addNewEmployee );
 
-    $( '#table-last-name' ).append( `<td>${lastName}</td>`);
-    $( '#name-in' ).val( '' );
-
-    $( '#table-identify' ).append( `<td>${identify}</td>`);
-    $( '#name-in' ).val( '' );
-
-    $( '#table-title' ).append( `<td>${jobTitle}</td>`);
-    $( '#name-in' ).val( '' );
-
-    $( '#table-salary' ).append( `<td>${salary}</td>`);
-    $( '#name-in' ).val( '' );
-
+$( '#name-in' ).val( '' );
+$( '#last-name').val( '' );
+$( '#identify' ).val( '' );
+$( '#job-title' ).val( '' );
+$( '#salary' ).val( '' );
 }
