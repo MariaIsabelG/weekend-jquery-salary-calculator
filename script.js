e305a30ecd28cba1026ff0ea2131ed82e1db9703
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-06-17 19:06:58 -0500

    starting calcTotal

--- script.js
+++ script.js
@@ -9,19 +9,20 @@ $( 'tbody' ).on( 'click', '.delete-btn', deleteEmployee );
 
 }
 
+
+
 // collects input data
 // outputs data in a table
 function addEmployee(){
 
     //let newEmployee = [name, lastName, identify, jobTitle, salary] 
+let name = $( '#name-in' ).val();
+let lastName = $( '#last-name').val();
+let identify = $( '#identify' ).val();
+let jobTitle = $( '#job-title' ).val();
+let salary = $( '#salary' ).val();
 
-    let name = $( '#name-in' ).val();
-    let lastName = $( '#last-name').val();
-    let identify = $( '#identify' ).val();
-    let jobTitle = $( '#job-title' ).val();
-    let salary = $( '#salary' ).val();
-
-    let addNewEmployee = `<tr class="new-row"><td>${name}</td>, <td>${lastName}</td>, <td>${identify}</td>, <td>${jobTitle}</td>, <td>${salary}</td>, <td><button class="delete-btn">DELETE</button></td></tr>`;
+let addNewEmployee = `<tr class="new-row"><td>${name}</td>, <td>${lastName}</td>, <td>${identify}</td>, <td>${jobTitle}</td>, <td>${salary}</td>, <td><button class="delete-btn">DELETE</button></td></tr>`;
 
 $( 'tbody' ).append( addNewEmployee );
 
@@ -37,3 +38,7 @@ function deleteEmployee(){
  $(this).closest ('tr').remove ();
 
 };
+
+function calcTotal(){
+    $( '#salary' ).
+}
